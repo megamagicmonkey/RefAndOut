b7cf3a004d0dcc13c43d7c7e070611f01365e8c7
Whit Stroup <email@example.com>
2021-08-03 16:51:22 -0500

    added instructions

--- RefandOut/Program.cs
+++ RefandOut/Program.cs
@@ -4,18 +4,19 @@ namespace RefandOut
 {
     class Program
     {
-        //TODO impliment this ref method by returning  a string and mulitplying the parameter. So it will have two outputs but only returns the string.
-        //TODO Call the method in the main and make sure the method changes the number of a initialized variable.
+        //TODO impliment this ref method by returning  a string and mulitplying the parameter.
+        //So it will have two outputs. The string will return the multiplication in sentence form. Also set x to equal itself times 5.
+        //TODO Call the method in the main and make sure the method changes the number of a initialized variable and returns a string.
         public static string TimesFive(ref int x)
         {
-  
+
         }
 
         //TODO create method that uses the out keyword. Call the method in the main.
-  
+
         static void Main(string[] args)
         {
-        
+
         }
 
     }
