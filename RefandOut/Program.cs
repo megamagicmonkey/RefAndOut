005850fe4149780e54b9ac1a255d884d770c65f9
Whit Stroup <email@example.com>
2021-09-03 14:20:32 -0500

    changed instructions

--- RefandOut/Program.cs
+++ RefandOut/Program.cs
@@ -4,19 +4,28 @@ namespace RefandOut
 {
     class Program
     {
-        //TODO impliment this ref method by returning  a string and mulitplying the parameter.
-        //So it will have two outputs. The string will return the multiplication in sentence form. Also set x to equal itself times 5.
-        //TODO Call the method in the main and make sure the method changes the number of a initialized variable and returns a string.
-        public static string TimesFive(ref int x)
+        //TODO impliment this ref method by returning a bool and subtracting from the parameter.
+        //So it will have two outputs. the bool of true if health is > 0. and also set x to decrease by 1.
+        //TODO Call the method in the main and make sure the method changes the number of a initialized variable and returns a bool.
+        public static bool SubtractHealth(ref int x)
         {
-
+      
         }
 
         //TODO create method that uses the out keyword. Call the method in the main.
+        
+
+        public int Sum(int x, int y, out  bool even)
+        {
+            //use this metho or make your own.
+
+        }
+
 
         static void Main(string[] args)
         {
 
+    
         }
 
     }
