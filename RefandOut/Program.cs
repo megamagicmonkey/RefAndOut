361e582871a0e5268bccd75e7d06049d635e30de
megamagicmonkey <megamagicmonkey@gmail.com>
2021-09-07 14:03:26 -0500

    Resolved

--- RefandOut/Program.cs
+++ RefandOut/Program.cs
@@ -9,22 +9,79 @@ namespace RefandOut
         //TODO Call the method in the main and make sure the method changes the number of a initialized variable and returns a bool.
         public static bool SubtractHealth(ref int x)
         {
-      
+            Console.WriteLine("The hero took damage!");
+            x--;
+            if (x > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         //TODO create method that uses the out keyword. Call the method in the main.
         
 
-        public int Sum(int x, int y, out  bool even)
+        public static int Sum(int x, int y, out  bool even)
         {
-            //use this metho or make your own.
+            Console.WriteLine($"Adding {x} and {y}");
+            if ((x + y) % 2 == 0)
+            {
+                even = true;
+            }
+            else
+            {
+                even = false;
+            }
 
+            return x + y;
+
+        }
+
+        public static void DisplaySum(int x, bool even)
+        {
+            if (even)
+            {
+                Console.WriteLine($"The resulting number of the last Sum() method is {x}, which is even.");
+            }
+            else
+            {
+                Console.WriteLine($"The resulting number of the last Sum() method is {x}, which is odd.");
+            }
+        }
+
+        public static void HeroStatus(bool alive)
+        {
+            if (alive)
+            {
+                Console.WriteLine("The hero is still going!");
+            }
+            else
+            {
+                Console.WriteLine("The hero has fallen...");
+            }
         }
 
 
         static void Main(string[] args)
         {
+            int health = 1;
+            bool alive = true;
+            bool even;
+            int sum1;
+            int sum2;
 
+            sum1 = Sum(1, 2, out even);
+            DisplaySum(sum1, even);
+            sum2 = Sum(2, 2, out even);
+            DisplaySum(sum2, even);
+
+
+            HeroStatus(alive);
+            alive = SubtractHealth(ref health);
+            HeroStatus(alive);
     
         }
 
